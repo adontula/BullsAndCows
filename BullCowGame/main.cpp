042d128a6861df7a95f1367c0b6e16885fef63d8
Aman Dontula <adontula@utexas.edu>
2018-02-25 16:05:33 -0600

    code cleanup

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,22 +3,39 @@
 
 using namespace std;
 
-int main() {
-	constexpr int WORD_LENGTH = 5;
+void PrintIntro();
+void PlayGame();
+string GetGuess();
 
+// entry point for the application
+int main() { 
+	PrintIntro();
+	PlayGame();
+	return 0;
+}
+
+void PlayGame() {
+	constexpr int NUM_GUESSES = 5;
+	for (int i = 0; i < NUM_GUESSES; i++) {
+		string Guess = GetGuess();
+		// repeat the guess back to them
+		cout << "Your guess was " << Guess << endl;
+		cout << endl;
+	}
+}
+
+void PrintIntro() {
 	// introduce the game
+	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORD_LENGTH << 
+	cout << "Can you guess the " << WORD_LENGTH <<
 		" letter isogram I'm thinking of?\n\n";
+}
 
-	//get a guess from the player
+//get a guess from the player
+string GetGuess() {
 	cout << "Enter your guess: ";
 	string Guess = "";
-	cin >> Guess;
-	
-	// repeat the guess back to them
-	cout << "Your guess was " << Guess << endl;
-
-	cout << endl;
-	return 0;
+	getline(cin, Guess);
+	return Guess;
 }
